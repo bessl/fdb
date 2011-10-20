d86b17b2cbd80894a50f8f2417bfde9536383f16
Bernhard Essl <me@bernhardessl.com>
2011-10-20 12:11:02 +0200

    Updated testcase for creating a tag

--- src/main/java/model/Tag.java
+++ src/main/java/model/Tag.java
@@ -3,23 +3,31 @@ package model;
 public class Tag {
 	
 	private String title;
-	private String url;
+	private String slug;
 	
 	public Tag(String title) {
 		setTitle(title);
+		setSlug();
 	}
 	
 	public String getTitle() {
 		return title;
 	}
 	public void setTitle(String title) {
+		if (title == null)
+			throw new IllegalArgumentException("Tag hat keinen Titel");
 		this.title = title;
 	}
-	public String getUrl() {
-		return url;
+	public String getSlug() {
+		return slug;
 	}
-	public void setUrl(String url) {
-		this.url = url;
+	public void setSlug() {
+		slug = getTitle().toLowerCase();
+		slug = slug.replace(" ", "-");
+		slug = slug.replace("--", "-");
+		slug = slug.replaceAll("[^a-z0-9-]", "");
+		if (slug.length() > 10) 
+			slug = slug.substring(0, 10);
 	}
 
 }
--- src/test/java/model/WhenCreatingATag.java
+++ src/test/java/model/WhenCreatingATag.java
@@ -3,51 +3,20 @@ package model;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class WhenCreatingATag {
 
-	private Movie movie;
-
-	@Before
-	public void setUp() throws Exception {
-		movie = new Movie();
-	}
-
-	// Slug - automatisch URL aus Titel erstellen.
+	  @Test(expected = IllegalArgumentException.class)
+	  public void withWrongTagWeShouldGetAnException() {
+		  Tag t1 = new Tag(null);
+	  }
+		  
 	@Test
-	public void movieTitleShouldSetASlugAutomatically() {
-		String[][] titles = {
-				{ "Spam Terry Jones, Michael Spam Palin, John Spam", "spam-terry-jones-mic", },
-				{ "&*#$KADJA23SDdadsa@#$#$", "kadja23sddadsa" } ,
-				{ "x  Kcd", "x-kcd" } 
-		};
-		
-		for (String[] t : titles) {
-			movie.setTitle(t[0]);
-			assertThat(movie.getSlug(), is(equalTo(t[1])));
-		}
-
-	}
-
-	// Rating
-
-	@Test(expected = IllegalArgumentException.class)
-	public void withWrongRatingValueWeShouldGetAnException() {
-		movie.setRating(1000);
-	}
-
-	@Test
-	public void newMovieShouldHaveDefaultRating() {
-		assertThat("Default Rating", movie.getRating(), equalTo(3));
-	}
-
-	@Test
-	public void updateMovieRating() {
-		movie.setRating(2);
-
-		assertThat("Update Rating", movie.getRating(), equalTo(2));
+	public void tagTitleShouldSetAURLAutomatically() {
+		Tag t1 = new Tag("foo bar");
+		assertThat(t1.getTitle(), is(equalTo("foo bar")));
+		assertThat(t1.getSlug(), is(equalTo("foo-bar")));
 	}
 
 }
