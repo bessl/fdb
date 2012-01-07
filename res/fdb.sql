9adeb57c176ff5f08576aa79b0c4085a8e4604af
Bernhard Essl <me@bernhardessl.com>
2012-01-07 16:02:33 +0100

    Updated SQL structure file

--- res/fdb.sql
+++ res/fdb.sql
@@ -23,6 +23,41 @@ CREATE TABLE IF NOT EXISTS `movies` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `movie_tags`
+--
+
+CREATE TABLE IF NOT EXISTS `movie_tags` (
+  `movie_id` int(11) NOT NULL,
+  `tag_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `playlists`
+--
+
+CREATE TABLE IF NOT EXISTS `playlists` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `playlist_movies`
+--
+
+CREATE TABLE IF NOT EXISTS `playlist_movies` (
+  `movie_id` int(11) NOT NULL,
+  `playlist_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tags`
 --
 
@@ -31,4 +66,5 @@ CREATE TABLE IF NOT EXISTS `tags` (
   `title` varchar(255) NOT NULL,
   `slug` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=19 ;
+
