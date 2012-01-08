e0cc5dbeef2b1f6b648fc5fcb3d69e04b0d68a00
Bernhard Essl <me@bernhardessl.com>
2012-01-08 18:31:19 +0100

    clean up

--- src/test/java/model/WhenCreatingAMovie.java
+++ src/test/java/model/WhenCreatingAMovie.java
@@ -1,11 +1,6 @@
 package model;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Before;
-import org.junit.Test;
 
 public class WhenCreatingAMovie {
 
@@ -20,16 +15,6 @@ public class WhenCreatingAMovie {
 		m3 = new Movie();
 		m3.setTitle("bla 3");
 
-	}
-
-	@Test
-	public void addMoviesToPlaylist() {
-		Playlist pl = new Playlist("Meine neue Playlist");
-		pl.addMovie(m1);
-		pl.addMovie(m2);
-		pl.addMovie(m3);
-		
-		assertEquals(3, pl.getMovies().size());
 	}
 
 }
