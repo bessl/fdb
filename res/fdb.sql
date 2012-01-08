afcc478e3e1d86cdfaa4068031fe8964af4a304b
Bernhard Essl <me@bernhardessl.com>
2012-01-08 18:25:14 +0100

    Updated sql structure file

--- res/fdb.sql
+++ res/fdb.sql
@@ -1,70 +1,45 @@
---
--- Database: `fdb`
---
+# Dump of table movie_tags
+# ------------------------------------------------------------
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `movie_tags`;
 
---
--- Table structure for table `movies`
---
-
-CREATE TABLE IF NOT EXISTS `movies` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
-  `url` varchar(255) NOT NULL,
-  `url_thumbnail` varchar(255) NOT NULL,
-  `url_player` varchar(255) NOT NULL,
-  `slug` varchar(255) NOT NULL,
-  `summary` text NOT NULL,
-  `rating` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `movie_tags`
---
-
-CREATE TABLE IF NOT EXISTS `movie_tags` (
+CREATE TABLE `movie_tags` (
   `movie_id` int(11) NOT NULL,
   `tag_id` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `playlists`
---
 
-CREATE TABLE IF NOT EXISTS `playlists` (
+# Dump of table movies
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `movies`;
+
+CREATE TABLE `movies` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
-  `slug` varchar(255) NOT NULL,
+  `url` varchar(255) DEFAULT '',
+  `url_thumbnail` varchar(255) DEFAULT '',
+  `url_player` varchar(255) DEFAULT '',
+  `slug` varchar(255) DEFAULT '',
+  `summary` text,
+  `rating` int(11) NOT NULL DEFAULT '3',
+  `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `playlist_movies`
---
-
-CREATE TABLE IF NOT EXISTS `playlist_movies` (
-  `movie_id` int(11) NOT NULL,
-  `playlist_id` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `tags`
---
 
-CREATE TABLE IF NOT EXISTS `tags` (
+# Dump of table tags
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `tags`;
+
+CREATE TABLE `tags` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
   `slug` varchar(255) NOT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=19 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
